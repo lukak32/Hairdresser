de8bfd55dac36bad34781080cc59d1c07d2a95c9
lukak32 <42774534+lukak32@users.noreply.github.com>
2020-05-10 20:24:33 +0200

    Update burger-menu.js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -3,3 +3,16 @@ document.getElementsByClassName("burger")[0].addEventListener("click", function(
         document.getElementsByClassName("nav-list")[0].classList.toggle("nav-list_active");
         document.getElementsByClassName("navigation__phone")[0].classList.toggle("navigation__phone_active");
 });
+
+var headerHeight = document.getElementById("header").offsetHeight;
+var navbar = document.getElementsByClassName("navigation")[0];
+
+function onScrollNav(){
+        if (window.pageYOffset >= headerHeight) {
+                navbar.classList.add("sticky");
+        } else {
+                navbar.classList.remove("sticky");
+        }
+}
+
+window.onscroll = function() {onScrollNav()};
